602f03a97e035dd8ab0d1977e8db7d3b289b7382
Christian Kellner <christian@kellner.me>
2014-06-25 11:01:07 +0200

    README.rst: Add basic usage

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name             = 'gca',
       version          = 0.1,
       author           = 'Christian Kellner',
       author_email     = 'kellner@bio.lmu.de',
+      url              = 'https://github.com/G-Node/GCA-Python',
       packages         = ['gca'],
       scripts          = ['gca-client', 'gca-filter', 'gca-lint',
                           'gca-select', 'gca-sort', 'gca-tex'])
