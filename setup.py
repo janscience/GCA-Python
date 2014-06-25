528fc46a9d41870fe78481822fc5f91908b516c0
Low Kian Seong <kianseong@gmail.com>
2014-06-25 19:01:15 +0800

    Update setup.py
    Adding one line to `setup.py` so that pypi search results and pip search results do not list project as `UNKNOWN`

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup
 setup(name             = 'gca',
       version          = 0.1,
       author           = 'Christian Kellner',
+      description     = 'Python client for the G-Node Conference Application Suite.',
       author_email     = 'kellner@bio.lmu.de',
       url              = 'https://github.com/G-Node/GCA-Python',
       packages         = ['gca'],
